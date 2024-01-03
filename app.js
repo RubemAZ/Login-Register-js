018b1510b79096c1b8bae429a1466d78b0a30822
RubemAZ <raz.rubem@gmail.com>
2024-01-02 22:00:56 -0300

    add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+/* Imports */
+
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+/* Open/Public - Route */
+app.get('/' , (req, res) =>{
+    res.status(200).json({msg: 'Welcome to API!'})
+})
+
+app.listen(3000)
